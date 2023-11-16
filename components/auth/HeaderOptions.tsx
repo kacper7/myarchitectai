65e57a2b08340cc41352439572dca048f31ce273
alabeco <alabecocliquebuster@gmail.com>
2023-11-16 14:55:55 +0300

    feat: redirect users to homepage on log out

--- components/auth/HeaderOptions.tsx
+++ components/auth/HeaderOptions.tsx
@@ -22,7 +22,7 @@ export default function AuthHeaderOptions() {
       const { error } = await supabase.auth.signOut();
 
       if (!error) {
-        window.location.reload();
+        window.location.href = "/";
       }
     } catch (error) {
       // TODO: Handle error
