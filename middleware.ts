87baf12cc0e293615fad0631c9b769a35d816cd0
alabeco <alabecocliquebuster@gmail.com>
2023-11-16 10:50:09 +0300

    fix: add signup url and force sign in

--- middleware.ts
+++ middleware.ts
@@ -1,24 +1,30 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
+const PRIVATE_URLS = [ // require authentication
+  '/generate',
+  '/generate-custom-architect',
+  '/generate-custom-home',
+  '/generate-floorplan',
+  '/generate-interior',
+]
+
 export async function middleware(req: NextRequest) {
   const res = NextResponse.next()
   const accessToken = req.cookies.get('sb.auth.token')?.value
 
-  if (!accessToken) {
-    return NextResponse.json({ error: 'Please sign in to perform this action' }, { status: 401 })
+  if (PRIVATE_URLS.includes(req.nextUrl.pathname)) {
+    if (!accessToken) {
+      return NextResponse.json({ error: 'Please sign in to perform this action' }, { status: 401 })
+    }
+  }
+
+  if (req.nextUrl.pathname === '/signup') {
+    if (accessToken) {
+      return NextResponse.redirect(new URL('/dream', req.nextUrl.origin))
+    }
   }
 
   return res
-}
-
-export const config = {
-  matcher: [
-    '/generate',
-    '/generate-custom-architect',
-    '/generate-custom-home',
-    '/generate-floorplan',
-    '/generate-interior',
-  ]
 }
 
